a18641e425d4de365dc9e145c73810bb55e439dd
Leo Spratt <enchant97@users.noreply.github.com>
2024-01-31 14:00:42 +0000

    migrate to solidstart

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+/* @refresh reload */
+import { Router } from '@solidjs/router'
+import './app.css'
+import { FileRoutes } from '@solidjs/start'
+import Wrapper from './components/wrapper'
+
+export default function App() {
+  return (
+    <Router root={Wrapper}>
+      <FileRoutes />
+    </Router>
+  )
+}
---
+++ src/entry-server.tsx
@@ -0,0 +1,21 @@
+import { createHandler } from "@solidjs/start/entry";
+import { StartServer } from "@solidjs/start/server";
+
+export default createHandler(() => (
+  <StartServer
+    document={({ assets, children, scripts }) => (
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Teleprompter</title>
+          {assets}
+        </head>
+        <body>
+          <div id="app">{children}</div>
+          {scripts}
+        </body>
+      </html>
+    )}
+  />
+));
