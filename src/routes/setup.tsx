e5c8272ae72c13bd95ccec672e19175ae2fc3ccd
Leo Spratt <enchant97@users.noreply.github.com>
2024-02-02 15:40:37 +0000

    ability to download/upload settings+script

--- src/routes/setup.tsx
+++ src/routes/setup.tsx
@@ -16,6 +16,28 @@ export default function Setup() {
       }}
       class="flex flex-col gap-2"
     >
+      <button onClick={() => {
+        let a = window.document.createElement("a")
+        a.href = URL.createObjectURL(new Blob(
+          [JSON.stringify(settings)],
+          { type: "application/json" },
+        ))
+        a.download = "script.json"
+        a.click()
+      }} class="btn">Download (JSON)</button>
+      <button onClick={() => {
+        let input = window.document.createElement("input")
+        input.type = "file"
+        input.accept = "application/json"
+        input.addEventListener("change", async () => {
+          if (input.files?.length === 1) {
+            let file = input.files[0]
+            setSettings(JSON.parse(await file.text()))
+            save()
+          }
+        })
+        input.click()
+      }} class="btn">Upload (JSON)</button>
       <label class="form-control">
         <span class="label-text">Auto Scroll Interval</span>
         <div class="flex gap-1 items-center">
