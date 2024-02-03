7ffe157fe74a47ccba9eb863837b1146d9fdb3fa
Leo Spratt <enchant97@users.noreply.github.com>
2024-02-03 17:45:26 +0000

    move ui routes

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -6,8 +6,8 @@ export default function Wrapper(props: ParentProps) {
     <div class="max-h-screen flex flex-col p-2 gap-2">
       <div class="flex gap-2">
         <A class="btn" href="/remote">Remote</A>
-        <A class="btn" href="/setup">Setup</A>
-        <A class="btn" href="/view">Viewer</A>
+        <A class="btn" href="/prompter">Prompter</A>
+        <A class="btn" href="/prompter/setup">Setup Prompter</A>
       </div>
       {props.children}
     </div>
