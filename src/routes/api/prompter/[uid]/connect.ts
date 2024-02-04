222e60d040407c3851bd787dd00d2b12c212322a
Leo Spratt <enchant97@users.noreply.github.com>
2024-02-04 21:03:09 +0000

    validate valid uid

--- src/routes/api/prompter/[uid]/connect.ts
+++ src/routes/api/prompter/[uid]/connect.ts
@@ -2,8 +2,15 @@ import { type APIEvent } from "@solidjs/start/server";
 import { subscribeForCommands } from "~/core/redis";
 import { RemoteCommand } from "~/core/types";
 
+const UUID_LOWER_RE = /^[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}$/
+
 export async function GET(ev: APIEvent) {
   let clientUid = ev.params.uid
+
+  if (!UUID_LOWER_RE.test(clientUid)) {
+    return new Response("400, Invalid Client Uid", { status: 400 })
+  }
+
   ev.node.res.writeHead(200, {
     "Content-Type": "text/event-stream",
     "Cache-Control": "no-cache",
